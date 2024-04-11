b9259c7f80f85da8c2f8a27e45427a867f0798b5
Aleksandra Zokova <teevoids@gmail.com>
2024-04-11 09:44:12 +0300

    db on tehtud

--- lille.sql
+++ lille.sql
@@ -2,3 +2,15 @@ CREATE DATABASE lillemyykZokova;
 USE lillemyykZokova;
 
 --tabel tootaja
+CREATE TABLE tootaja(
+tootajaID int Primary key identity(1,1),
+eesnimi varchar(20),
+perekonnanimi varchar(20),
+isikukood varchar(11) UNIQUE
+)
+SELECT * FROM tootaja;
+
+INSERT INTO tootaja(
+eesnimi, perekonnanimi, isikukood)
+VALUES(
+'Aleksandra', 'Zokova', '60604167056');
